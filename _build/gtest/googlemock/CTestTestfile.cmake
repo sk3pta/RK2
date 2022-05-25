e75244ab526816315ed035583b692bf98a99f918
sk3pta <nnnnnmmmmm092@protonmail.com>
2022-05-25 21:43:38 +0300

    cmakelists

---
+++ _build/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/augustus/TIMP/RK2/third-party/gtest/googlemock
+# Build directory: /home/augustus/TIMP/RK2/_build/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
