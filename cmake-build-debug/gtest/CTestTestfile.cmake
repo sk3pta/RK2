6474ddd8aedd9d8a817350ff79d1bd7f51ad828c
sk3pta <nnnnnmmmmm092@protonmail.com>
2022-05-25 20:52:53 +0300

    init commit

---
+++ cmake-build-debug/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/augustus/TIMP/RK2/third-party/gtest
+# Build directory: /home/augustus/TIMP/RK2/cmake-build-debug/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ cmake-build-debug/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/augustus/TIMP/RK2/third-party/gtest/googletest
+# Build directory: /home/augustus/TIMP/RK2/cmake-build-debug/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
